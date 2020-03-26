d33cb575d5d655d229678ef6d6374137223ef5eb
munsifmulla <munsifmulla@gmail.com>
2020-03-27 01:09:19 +0530

    Admin login URL change

--- dashboard/js/script.js
+++ dashboard/js/script.js
@@ -9,7 +9,7 @@ var urls = {
   search: variables.API_BASE + 'api/find-log',
   create: variables.API_BASE + 'api/create-log',
   registerUser: variables.API_BASE + 'api-user/register',
-  loginUser: variables.API_BASE + 'api-user/login',
+  adminLogin: variables.API_BASE + 'api-admin/login',
   collection: {
     create: variables.API_BASE + 'api/collection/add',
   }
